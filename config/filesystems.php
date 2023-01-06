2fd5a29279f1382da352dabcb65523dcca838924
Mickael Ndinga <ndingamickael@gmail.com>
2023-01-06 08:37:25 +0100

    #27 filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,15 +36,11 @@ return [
             'root' => tmp_path(),
             'visibility' => 'public',
         ],
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        'dropbox' => [
+            'driver' => 'dropbox',
+            "client_id" => 'test',
+            "client_secret" => 'test',
+            "access_token" => 'test'
         ]
     ],
 
