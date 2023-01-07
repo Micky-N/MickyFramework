fab83b7791bb1327db18f4e21733a86fdf985f8e
Mickael Ndinga <ndingamickael@gmail.com>
2023-01-07 01:26:05 +0100

    #27 finish

--- config/cache.php
+++ config/cache.php
@@ -9,27 +9,27 @@
  */
 return [
     /*
-    |--------------------------------------------------------------------------
-    | Default Filesystem space
-    |--------------------------------------------------------------------------
-    |
-    | You can specify the default filesystem space that should be used
-    | by the framework.
-    |
-    */
+     *--------------------------------------------------------------------------
+     * Default Filesystem space
+     *--------------------------------------------------------------------------
+     *
+     * You can specify the default filesystem space that should be used
+     * by the framework.
+     *
+     */
     'default' => 'app',
 
     /*
-    |--------------------------------------------------------------------------
-    | Filesystem Spaces
-    |--------------------------------------------------------------------------
-    |
-    | You can configure as many filesystem spaces as you wish, and you
-    | may even configure multiple spaces of the same driver.
-    |
-    | Supported Drivers: "local"
-    |
-    */
+     *--------------------------------------------------------------------------
+     * Filesystem Spaces
+     *--------------------------------------------------------------------------
+     *
+     * You can configure as many filesystem spaces as you wish, and you
+     * may even configure multiple spaces of the same driver.
+     *
+     * Supported Drivers: "local"
+     *
+     */
     'spaces' => [
         'app' => [
             'driver' => 'local',
