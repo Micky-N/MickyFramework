bd54ced32339d3c2983edf60c74e10a26fab6299
Mickael Ndinga <ndingamickael@gmail.com>
2023-01-07 02:31:29 +0100

    #26 vuejs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     SyntaxHighlighter.defaults['toolbar'] = false;
     SyntaxHighlighter.all();
 
 });
+
+function getCurrentPage() {
+    const url = new URL(window.location.href);
+    return url.searchParams.get('p')
+}
+
+function changePage(page = 'home') {
+    const url = new URL(window.location.href);
+    let newUrl = url.origin
+    window.location.href = newUrl + `?p=${page}`
+}
+
+
+var app = new Vue({
+    el: '#app',
+})
