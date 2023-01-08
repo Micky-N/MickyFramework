d77aa8433a9cdc2242c5b0b326372ead07186fe5
Mickael Ndinga <ndingamickael@gmail.com>
2023-01-08 17:24:57 +0100

    #26 dynamic page

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 
 function getCurrentPage() {
     const url = new URL(window.location.href);
-    return url.searchParams.get('p')
+    return url.searchParams.get('p') ?? 'home'
 }
 
 function changePage(page = 'home') {
@@ -16,7 +16,29 @@ function changePage(page = 'home') {
     window.location.href = newUrl + (page === 'home' ? '' : `?p=${page}`)
 }
 
+function template(view) {
+    return 'views/' + view + '.html'
+}
 
-var app = new Vue({
-    el: '#app',
+function mount(el) {
+    let view = getCurrentPage()
+    let file = template(view)
+    $.each(el, function () {
+        $(this).load(file)
+    })
+}
+
+$(function () {
+    let includes = $('[data-include]')
+    $.each(includes, function () {
+        let view = $(this).data('include')
+        let file = template('includes/' + view)
+        $(this).load(file)
+    })
+})
+
+
+$(function () {
+    let el = $('#app')
+    mount(el)
 })
